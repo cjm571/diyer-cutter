56a434204f5fc4eb8b405b9e6c8fb39140634d76
CJ McAllister <cjm571@gmail.com>
2022-06-26 22:48:15 -0600

    Kindof-working barebones RTIC implementation - Updated to microbit 0.13.0

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,13 @@ Copyright (C) 2022 CJ McAllister
 
 #![no_main]
 #![no_std]
-#![feature(default_alloc_error_handler)]
 
 use core::{cell::RefCell, ops::DerefMut};
 
-extern crate alloc;
-use alloc::boxed::Box;
+// extern crate alloc;
+// use alloc::boxed::Box;
 
-use cortex_m::interrupt::{free, Mutex};
-use cortex_m_rt::entry;
+
 use microbit::{
     board::Board,
     hal::{
@@ -39,41 +37,49 @@ use microbit::{
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
-pub mod microbit_allocator;
+// pub mod microbit_allocator;
 
 pub mod lcd1602;
 use lcd1602::{Lcd1602, LcdInputPins};
 
-pub mod motors;
-use motors::MotorDC;
+use rtic::app;
 
-static G_TIMER0: Mutex<RefCell<Option<Timer<TIMER0>>>> = Mutex::new(RefCell::new(None));
-static G_TIMER1: Mutex<RefCell<Option<Timer<TIMER1>>>> = Mutex::new(RefCell::new(None));
-static G_LCD: Mutex<RefCell<Option<Lcd1602>>> = Mutex::new(RefCell::new(None));
+#[app(device = microbit::pac, peripherals = true, dispatchers = [SWI0_EGU0])]
+mod app {
+    use super::*;
 
+    ///////////////////////////////////////////////////////////////////////////////
+    //  Named Constants
+    ///////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////
-//  Named Constants
-///////////////////////////////////////////////////////////////////////////////
-
-const ONE_SECOND_IN_MHZ: u32 = 1000000;
+    const ONE_SECOND_IN_MHZ: u32 = 1000000;
 
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
+    #[shared]
+    struct Shared {
+        timer0: Timer<TIMER0>,
+        // lcd: Lcd1602,
+    }
 
-    // Take ownership of the full board
-    let board = Board::take().unwrap();
+    #[local]
+    struct Local {
+        timer1: Timer<TIMER1>,
+    }
 
-    let _test_box = Box::new(5);
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
 
-    // Instantiate a timer
-    let mut timer0 = Timer::new(board.TIMER0);
+        // Take ownership of the full board
+        let board = Board::new(cx.device, cx.core);
 
-    // Initialize a 1-second timer
-    let mut timer1 = Timer::new(board.TIMER1);
-    free(|cs| {
+        // let _test_box = Box::new(5);
+
+        // Instantiate a timer
+        let mut timer0 = Timer::new(board.TIMER0);
+
+        // Initialize a 1-second timer
+        let mut timer1 = Timer::new(board.TIMER1);
         // Stop and clear the timer before configuring it
         timer1.task_stop().write(|w| w.tasks_stop().trigger());
         timer1.task_clear().write(|w| w.tasks_clear().trigger());
@@ -93,123 +99,104 @@ fn main() -> ! {
         // Start the timer
         timer1.start(ONE_SECOND_IN_MHZ);
 
-        G_TIMER1.borrow(cs).replace(Some(timer1));
-    });
+        // // Instantiate the LCD and initialize
+        // let input_pins = LcdInputPins::new(
+        //     [
+        //         board.pins.p0_02.into_push_pull_output(Level::Low).degrade(), // P0
+        //         board.pins.p0_03.into_push_pull_output(Level::Low).degrade(), // P1
+        //         board.pins.p0_04.into_push_pull_output(Level::Low).degrade(), // P2
+        //         board
+        //             .display_pins
+        //             .col3
+        //             .into_push_pull_output(Level::Low)
+        //             .degrade(), // P3
+        //         board
+        //             .display_pins
+        //             .col1
+        //             .into_push_pull_output(Level::Low)
+        //             .degrade(), // P4
+        //         board
+        //             .buttons
+        //             .button_a
+        //             .into_push_pull_output(Level::Low)
+        //             .degrade(), // P5
+        //         board
+        //             .display_pins
+        //             .col4
+        //             .into_push_pull_output(Level::Low)
+        //             .degrade(), // P6
+        //         board
+        //             .display_pins
+        //             .col2
+        //             .into_push_pull_output(Level::Low)
+        //             .degrade(), // P7
+        //     ],
+        //     board.pins.p0_09.into_push_pull_output(Level::Low).degrade(), // P9
+        //     board
+        //         .display_pins
+        //         .col5
+        //         .into_push_pull_output(Level::Low)
+        //         .degrade(), // P10
+        //     board.pins.p0_01.into_push_pull_output(Level::Low).degrade(), // P14
+        // );
+        // let mut lcd = Lcd1602::new(input_pins);
+        // lcd.initialize(&mut timer0);
 
-    // Instantiate the LCD and initialize
-    let input_pins = LcdInputPins::new(
-        [
-            board.pins.p0_02.into_push_pull_output(Level::Low).degrade(), // P0
-            board.pins.p0_03.into_push_pull_output(Level::Low).degrade(), // P1
-            board.pins.p0_04.into_push_pull_output(Level::Low).degrade(), // P2
-            board
-                .display_pins
-                .col3
-                .into_push_pull_output(Level::Low)
-                .degrade(), // P3
-            board
-                .display_pins
-                .col1
-                .into_push_pull_output(Level::Low)
-                .degrade(), // P4
-            board
-                .buttons
-                .button_a
-                .into_push_pull_output(Level::Low)
-                .degrade(), // P5
-            board
-                .display_pins
-                .col4
-                .into_push_pull_output(Level::Low)
-                .degrade(), // P6
-            board
-                .display_pins
-                .col2
-                .into_push_pull_output(Level::Low)
-                .degrade(), // P7
-        ],
-        board.pins.p0_09.into_push_pull_output(Level::Low).degrade(), // P9
-        board
-            .display_pins
-            .col5
-            .into_push_pull_output(Level::Low)
-            .degrade(), // P10
-        board.pins.p0_01.into_push_pull_output(Level::Low).degrade(), // P14
-    );
-    let mut lcd = Lcd1602::new(input_pins);
-    free(|cs| {
-        lcd.initialize(&mut timer0);
-
-        // Greet the user
-        lcd.display_greeting(&mut timer0);
-
-        G_TIMER0.borrow(cs).replace(Some(timer0));
-        G_LCD.borrow(cs).replace(Some(lcd));
-    });
-
-    // Initialize the "DC motor"
-    let pwm0 = Pwm::new(board.PWM0);
-    let motor_dc = MotorDC::new(
-        board.pins.p1_02.into_push_pull_output(Level::Low).degrade(),
-        &pwm0,
-    );
-    motor_dc.initialize();
-
-    rprintln!("Entering main loop");
-    loop {
-        if board.buttons.button_b.is_low().unwrap() {
-            rprintln!("BTN_B Pressed!");
-        }
-
-        free(|cs| {
-            if let Some(ref mut timer0) = G_TIMER0.borrow(cs).borrow_mut().deref_mut() {
-                timer0.delay_ms(10_u32);
-            }
-        });
+        // // Greet the user
+        // lcd.display_greeting(&mut timer0);
+        
+        (
+            Shared {
+                timer0,
+                // lcd,
+            },
+            Local { timer1 },
+            init::Monotonics()
+        )
     }
-}
 
+    
+    #[idle(shared = [timer0])]
+    fn idle(mut cx: idle::Context) -> ! {
+        rprintln!("Entering main loop");
+        
+        let mut count = 0;
+        loop {            
+            cx.shared.timer0.lock(|timer0| {
+                timer0.delay_ms(10_u32);
+            });
 
-#[interrupt]
-fn TIMER1() {
-    static mut COUNTER: u8 = 0;
-    rprintln!("TIMER1 INTERRUPT!");
-
-    // Clear the timer interrupt flag
-    free(|cs| {
-        if let Some(ref mut timer1) = G_TIMER1.borrow(cs).borrow_mut().deref_mut() {
-            timer1
-                .event_compare_cc0()
-                .write(|w| w.events_compare().not_generated());
+            count += 1;
+            rprintln!("Loop #{}", count);
         }
-    });
+    }
 
-    // Cycle through values on the LCD
-    free(|cs| {
-        if let (Some(ref mut lcd), Some(ref mut timer0)) = (
-            G_LCD.borrow(cs).borrow_mut().deref_mut(),
-            G_TIMER0.borrow(cs).borrow_mut().deref_mut(),
-        ) {
-            lcd.backspace(3, timer0);
 
-            #[allow(unused_unsafe)]
-            unsafe {
-                lcd.write_u8(*COUNTER, timer0);
-            }
+    #[task(shared = [timer0, /*lcd*/], local = [timer1])]
+    fn timer1(cx: timer1::Context) {
+        static mut COUNTER: u8 = 0;
+        rprintln!("TIMER1 INTERRUPT!");
+
+        // Clear the timer interrupt flag
+        cx.local.timer1.event_compare_cc0()
+            .write(|w| w.events_compare().not_generated());
+
+        // // Cycle through values on the LCD
+        // (cx.shared.timer0, cx.shared.lcd).lock(|timer0, lcd| {
+        //     lcd.backspace(3, timer0);
+            
+        //     #[allow(unused_unsafe)]
+        //     unsafe {
+        //         lcd.write_u8(COUNTER, timer0);
+        //     }
+        // });
+
+        // Start another 1-second timer
+        cx.local.timer1.start(ONE_SECOND_IN_MHZ);
+
+        #[allow(unused_unsafe)]
+        unsafe {
+            COUNTER += 1;
         }
-    });
-
-
-    // Start another 1-second timer
-    free(|cs| {
-        if let Some(ref mut timer1) = G_TIMER1.borrow(cs).borrow_mut().deref_mut() {
-            // Start another 1-second timer
-            timer1.start(ONE_SECOND_IN_MHZ);
-        }
-    });
-
-    #[allow(unused_unsafe)]
-    unsafe {
-        *COUNTER += 1;
     }
 }
