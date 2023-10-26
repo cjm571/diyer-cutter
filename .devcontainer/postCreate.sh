42036bf84b4b0b117343bb8fcf013cf12323b9db
CJ McAllister <cjm571@gmail.com>
2023-10-25 19:56:11 -0600

    Removed extraneous devcontainer artifacts

--- .devcontainer/postCreate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Give ownership of the cargo registry to the vscode user to allow writes and such to work
-chown vscode:vscode /usr/local/cargo/registry
-
-cargo install flip-link
-cargo install probe-rs --features cli
